f2ab266a553609a591bbe6d0c89c2ebd86b1a1ce
Siddharth Cherukupalli <cherukusid@gmail.com>
2022-01-28 19:25:06 -0500

    Added initial pods

---
+++ EMS Resources App/Pods/Target Support Files/Pods-EMS Resources App-EMS Resources AppUITests/Pods-EMS Resources App-EMS Resources AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EMS_Resources_App_EMS_Resources_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EMS_Resources_App_EMS_Resources_AppUITests
+@end
---
+++ EMS Resources App/Pods/Target Support Files/Pods-EMS Resources App/Pods-EMS Resources App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EMS_Resources_App : NSObject
+@end
+@implementation PodsDummy_Pods_EMS_Resources_App
+@end
